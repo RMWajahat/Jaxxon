a66fe10dc5d0864fac5b6300d55696046f3b6560
RMWajahat <rajawajahat2003@gmail.com>
2024-01-01 22:29:02 +0500

    contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,8 +2,15 @@
 
 const Contact = () => {
   return (
-    <div>
-      Contact Page
+    <div className="contactpage">
+      <div className="contacthelp">
+        <h1 className="helpp">Contact <span>JAXXON</span></h1>
+        <iframe width="1123" height="632" src="https://www.youtube.com/embed/Ueq3Ib-vUUA" title="The Most Frequently Asked Questions About JAXXON Jewelry Answered." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </div>
+      <div className="morehelp">
+        <h1 className="stillhelp">Still Need Help?</h1>
+        <span>Couldn’t find the answer you needed? Our customer service is here to help!</span>
+      </div>
     </div>
   )
 }
