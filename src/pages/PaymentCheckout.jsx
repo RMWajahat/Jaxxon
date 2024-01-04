72f76a31649c2134035b93c6d0d7c77d09b71665
RMWajahat <rajawajahat2003@gmail.com>
2024-01-04 12:36:22 +0500

    card options

--- src/pages/PaymentCheckout.jsx
+++ src/pages/PaymentCheckout.jsx
@@ -17,6 +17,17 @@ const PaymentCheckout = () => {
           <small>Keep me up to date on news and exclusive offers</small>
         </div>
         </div>
+
+
+        <div className="cardinfo">
+        <h1 className="cardinfotag">Card Information</h1>
+        <input type="text" name="cardnum" id="cardnum" placeholder="1234 1234 1234 1234" pattern="[0-9]+" maxLength={16} required autoComplete="off" />
+        <div className="keepuptodate carddata">
+          <input type="text" name="expiry" placeholder="MM/YY" pattern="[0-9]+" maxLength={4} id=""  required autoComplete="off"  />
+          <input type="password" id="cardpin" placeholder="CVC" required autoComplete="off" maxLength={4}/>
+        </div>
+        </div>
+
         <div className="shipping_details">
         <h1 className="contacttag">Shipping address</h1>
         <input type="text" name="fname" id="firstname" placeholder="First name" required autoComplete="off" />
